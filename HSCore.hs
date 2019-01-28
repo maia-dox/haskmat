aceebffc47ebfe9039cc062981d434240c7328e6
Dox-Box <jddoxtad@syr.edu>
2019-01-28 02:32:36 -0500

    merge ver 0

---
+++ HSCore.hs
@@ -0,0 +1,16 @@
+module HSCore where
+
+import HSMatrix
+
+-- A Haskell Matrix and Vector Math library. 
+-- author Jim Doxtader
+
+
+<<<<<<< HEAD
+
+matrix1 = Matrix2f [[1.2, 1, 3, 4, 5, 6], [1,3,0,244], [0]] 
+=======
+-- a Vector / Matrix Math library for Haskell
+-- author : Jim Doxtader
+
+>>>>>>> d48ca86aaf62d604cd54ba85e09911862b67e2ef
