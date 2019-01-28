8f7b43daf2c1087d5889bee4b2cbbd9360771eb8
Jim Doxtader <43254987+jimdox@users.noreply.github.com>
2019-01-28 02:34:00 -0500

    Update HSCore.hs

--- HSCore.hs
+++ HSCore.hs
@@ -6,11 +6,5 @@ import HSMatrix
 -- author Jim Doxtader
 
 
-<<<<<<< HEAD
-
 matrix1 = Matrix2f [[1.2, 1, 3, 4, 5, 6], [1,3,0,244], [0]] 
-=======
--- a Vector / Matrix Math library for Haskell
--- author : Jim Doxtader
 
->>>>>>> d48ca86aaf62d604cd54ba85e09911862b67e2ef
