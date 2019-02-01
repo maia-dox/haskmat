16f52cdedfe0b6f8441777278534155cfc33cd6f
Dox-Box <jddoxtad@syr.edu>
2019-02-01 02:20:09 -0500

    restructuring codebase...

--- Core.hs
+++ Core.hs
@@ -1,10 +1,7 @@
 module Core where
 
-import Matrix
 
 -- A Haskell Matrix and Vector Math library. 
 -- author Jim Doxtader
 
-
-matrix1 = Matrix2f [[1.2, 1, 3, 4, 5, 6], [1,3,0,244], [0]] 
 
--- Haskmat.hs
+++ Haskmat.hs
@@ -1,6 +1,9 @@
 module Haskmat where
 import Core
-
+import Matrix
+import Vector
+import Stack
+import LinkedList
 
 
 
