b09e97ecb5b644046075275db7a9422ef1aecdb8
Jim Dox <jdoxtader@protonmail.com>
2019-03-27 13:01:14 -0400

    hex converted added

--- BitConvert.hs
+++ BitConvert.hs
@@ -1,8 +1,11 @@
 module BitConvert where
+import Data.Char
+-- int -> bit and hex utils
 
 
-uint_to_bit :: Int -> String
-uint_to_bit i = uint_convert i 128
+-- unsigned int -> String
+uint_to_bits :: Int -> String
+uint_to_bits i = uint_convert i 128
 	where 
 	uint_convert :: Int -> Int -> [Char]
 	uint_convert num m
@@ -10,8 +13,10 @@ uint_to_bit i = uint_convert i 128
 		| (num == m) || (num > m) = '1' : (uint_convert (num-m) (div m 2))
 		| num < m = '0' : (uint_convert num (div m 2))
 
-int_to_bit :: Int -> String
-int_to_bit i 
+
+-- converts signed int to bits
+int_to_bits :: Int -> String
+int_to_bits i 
 	| i >= 0 = '0' : (pos_convert i 64)
 	| otherwise = '1' : (carry_add ( flip_bits (pos_convert (-1 * i) 64)))
 
@@ -32,6 +37,35 @@ int_to_bit i
 		| c == '0' = cs ++ "1"
 		| c == '1' = (carry_add cs) ++ "0"
 		| otherwise = " OVERFLOW"
+
+
+-- converts bits to unsigned int
+bits_to_uint :: String -> Int
+bits_to_uint str = bit_convert str 128
+	where
+	bit_convert :: String -> Int -> Int
+	bit_convert [] _ = 0
+	bit_convert (x:xs) msb 
+		| x == '1' = msb + (bit_convert xs (div msb 2))
+		| x == '0' = bit_convert xs (div msb 2)
+
+
+
+-- converts hex string to int
+hex_to_int :: String -> Int
+hex_to_int [] = 0
+hex_to_int (c:cs)
+	| c == 'A' = (10 * (16^msb)) + (hex_to_int cs)
+	| c == 'B' = (11 * (16^msb)) + (hex_to_int cs)
+	| c == 'C' = (12 * (16^msb)) + (hex_to_int cs)
+	| c == 'D' = (13 * (16^msb)) + (hex_to_int cs)
+	| c == 'E' = (14 * (16^msb)) + (hex_to_int cs)
+	| c == 'F' = (15 * (16^msb)) + (hex_to_int cs)
+	| otherwise = (digitToInt c) + (hex_to_int cs)
+	where
+	msb = length cs
+
+
 
 
 
