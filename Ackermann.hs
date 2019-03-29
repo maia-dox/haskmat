fa05e3709d7e0e2bf474d6fe0292abf606e2a84a
Jim Dox <jdoxtader@protonmail.com>
2019-03-28 23:19:37 -0400

    added 'super ackermann' function

--- Ackermann.hs
+++ Ackermann.hs
@@ -5,8 +5,17 @@ module Ackermann where
 -- (m > 0 && n > 0): A(m-1, A(m,n-1))
 
 ackermann :: Integer -> Integer -> Integer
-ackermann m n 
-        | m == 0 = n + 1
-        | (m > 0) && (n == 0) = ackermann (m-1) 1
-        | (m > 0) && (n > 0) = ackermann (m-1) (ackermann m (n-1))
+ackermann 0 n = n + 1
+ackermann m 0 = (ackermann (m-1) 1)
+ackermann m n = ackermann (m-1) (ackermann m (n-1))
+
+
+
+
+-- **causes stack overflow**
+super_ack :: Integer -> Integer -> Integer
+super_ack 0 n = n + 1
+super_ack m 0 = super_ack (m-1) 1
+super_ack m n = super_ack (m-1) (super_ack m (super_ack m (n-1)) )
+
 
