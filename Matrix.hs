aed16208115da4ace171c43ff7289bf0ea23de57
Jim Doxtader <43254987+jimdox@users.noreply.github.com>
2019-08-15 15:22:31 -0400

    Update Matrix.hs

--- Matrix.hs
+++ Matrix.hs
@@ -9,11 +9,6 @@ data Matrix = Matrix1f [Float]
                 deriving(Show, Eq)
 
 
-
-
-
-
-
 -- take in two rows of data from a Matrix, and return the combined row. (Float)
 addMatRow :: [Float] -> [Float] -> [Float]
 addMatRow [] [] = []
@@ -39,12 +34,12 @@ addMatData4f :: [[[[Float]]]] -> [[[[Float]]]] -> [[[[Float]]]]
 addMatData4f [] [] = []
 addMatData4f (a:matA) (b:matB) = (addMatData3f a b) : (addMatData4f matA matB)
 
+
 addmat :: Matrix -> Matrix -> Matrix
 addmat (Matrix1f matA) (Matrix1f matB) = Matrix1f (addMatRow matA matB)
 addmat (Matrix2f matA) (Matrix2f matB) = Matrix2f (addMatData2f matA matB)
 addmat (Matrix3f matA) (Matrix3f matB) = Matrix3f (addMatData3f matA matB)
 addmat (Matrix4f matA) (Matrix4f matB) = Matrix4f (addMatData4f matA matB)
-
 
 
 subMatRow :: [Float] -> [Float] -> [Float]
@@ -53,11 +48,13 @@ subMatRow (a:rowA) (b:rowB) = (a - b) : (subMatRow rowA rowB)
 subMatRow [] rowB = rowB
 subMatRow rowA [] = rowA
 
+
 subMatData2f :: [[Float]] -> [[Float]] -> [[Float]]
 subMatData2f [] [] = []
 subMatData2f (a:matA) (b:matB) = (subMatRow a b) : (addMatData2f matA matB)
 subMatData2f [] matB = matB
 subMatData2f matA [] = matA
+
 
 subMatData3f :: [[[Float]]] -> [[[Float]]] -> [[[Float]]]
 subMatData3f [] [] = []
@@ -72,12 +69,12 @@ subMatData4f (a:matA) (b:matB) = (subMatData3f a b) : (subMatData4f matA matB)
 subMatData4f [] matB = matB
 subMatData4f matA [] = matA
 
+
 submat :: Matrix -> Matrix -> Matrix
 submat (Matrix1f matA) (Matrix1f matB) = Matrix1f (subMatRow matA matB)
 submat (Matrix2f matA) (Matrix2f matB) = Matrix2f (subMatData2f matA matB)
 submat (Matrix3f matA) (Matrix3f matB) = Matrix3f (subMatData3f matA matB)
 submat (Matrix4f matA) (Matrix4f matB) = Matrix4f (subMatData4f matA matB)
-
 
 
 -- multiplyMat
@@ -94,11 +91,13 @@ mulMatData2f (a:matA) (b:matB) = (mulMatRow a b) : (mulMatData2f matA matB)
 mulMatData2f [] matB = matB
 mulMatData2f matA [] = matA
 
+
 mulMatData3f :: [[[Float]]] -> [[[Float]]] -> [[[Float]]]
 mulMatData3f [] [] = []
 mulMatData3f (a:matA) (b:matB) = (mulMatData2f a b) : (mulMatData3f matA matB)
 mulMatData3f [] matB = matB
 mulMatData3f matA [] = matA
+
 
 mulMatData4f :: [[[[Float]]]] -> [[[[Float]]]] -> [[[[Float]]]]
 mulMatData4f [] [] = []
@@ -106,16 +105,12 @@ mulMatData4f (a:matA) (b:matB) = (mulMatData3f a b) : (mulMatData4f matA matB)
 mulMatData4f [] matB = matB
 mulMatData4f matA [] = matA
 
+
 mulMat :: Matrix -> Matrix -> Matrix
 mulMat (Matrix1f matA) (Matrix1f matB) = Matrix1f (mulMatRow matA matB)
 mulMat (Matrix2f matA) (Matrix2f matB) = Matrix2f (mulMatData2f matA matB)
 mulMat (Matrix3f matA) (Matrix3f matB) = Matrix3f (mulMatData3f matA matB)
 mulMat (Matrix4f matA) (Matrix4f matB) = Matrix4f (mulMatData4f matA matB)
-
-
-
-
-
 
 
 -- Vector * Matrix operations
@@ -128,10 +123,4 @@ convertVecMat (Vector1f x) = Matrix1f ([x])
 addMatVec :: Matrix -> Vector -> Matrix
 addMatVec (Matrix1f matA) (Vector1f x) = Matrix1f matA
 
-
-
-
-
-
 -- TODO : transformation functions 
-
